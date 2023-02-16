0a7f4af57c4220fc7e2a149963628854ea64355a
Nathan Mentley <nathan@Nathans-MacBook-Air.local>
2023-02-16 10:36:19 -0500

    0.0.8 update

--- Sources/AppInitializer.swift
+++ Sources/AppInitializer.swift
@@ -8,7 +8,7 @@ public final class AppInitializer: ServerInitializer {
 
     public override func initialize(for app: ServerApp, with configurationBuilder: ConfigurationBuilder) throws {
         _ = try app
-            .wireUp(singleton: { _ in return Bundle.module; })
+            .wireUp(singleton: { _, _ in return Bundle.module; })
 
             .wireUp(assetControllers: [
                 "/favicon.ico": "/Public/favicon.ico",
@@ -16,11 +16,11 @@ public final class AppInitializer: ServerInitializer {
                 "/assets/:path*": "/Public/"
             ])
 
-            .wireUp(controller: { sp in return AboutController(templatingEngine: try await sp.fetchType()); })
-            .wireUp(controller: { sp in return DocumentationController(templatingEngine: try await sp.fetchType()); })
-            .wireUp(controller: { sp in return GettingStartedController(templatingEngine: try await sp.fetchType()); })
-            .wireUp(controller: { sp in return IndexController(templatingEngine: try await sp.fetchType()); })
-            .wireUp(controller: { sp in return ReleasesController(templatingEngine: try await sp.fetchType()); })
-            .wireUp(controller: { sp in return SupportController(templatingEngine: try await sp.fetchType()); });
+            .wireUp(controller: { _, sp in return AboutController(templatingEngine: try await sp.fetchType()); })
+            .wireUp(controller: { _, sp in return DocumentationController(templatingEngine: try await sp.fetchType()); })
+            .wireUp(controller: { _, sp in return GettingStartedController(templatingEngine: try await sp.fetchType()); })
+            .wireUp(controller: { _, sp in return IndexController(templatingEngine: try await sp.fetchType()); })
+            .wireUp(controller: { _, sp in return ReleasesController(templatingEngine: try await sp.fetchType()); })
+            .wireUp(controller: { _, sp in return SupportController(templatingEngine: try await sp.fetchType()); });
     }
 }
